a58c074a835562842e58a59ba4b28b617c99f664
codytheroux96 <cody.theroux3@gmail.com>
2024-10-13 18:14:18 -0400

    added tabout and autopairs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,13 +3,36 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-    {
-      "fatih/vim-go",
-      ft = "go",
-      config = function()
-        -- Disable vim-go features that are replaced by LSP
-        vim.g.go_def_mapping_enabled = 0
-        vim.g.go_code_completion_enabled = 0
-      end
-    }
+  {
+    "fatih/vim-go",
+    ft = "go",
+    config = function()
+      -- Disable vim-go features that are replaced by LSP
+      vim.g.go_def_mapping_enabled = 0
+      vim.g.go_code_completion_enabled = 0
+    end
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require('nvim-autopairs').setup{}
+    end
+  },
+  {
+    "abecodes/tabout.nvim",
+    config = function()
+      require('tabout').setup {
+        tabkey = '<Tab>', -- Key to trigger tabout
+        backwards_tabkey = '<S-Tab>', -- Key to go backwards
+        act_as_tab = true, -- Enables the function of a normal tab in places where no pairs are found
+        enable_backwards = true, -- Allows backward navigation
+        completion = false, -- No interference with completion
+        ignore_beginning = false, -- Don't ignore the start of pairs
+        use_treesitter = true, -- Use treesitter to check for pairs
+        check_ts = true, -- Ensures compatibility with nvim-treesitter
+      }
+    end,
+    wants = { "nvim-treesitter" }, -- Ensures nvim-treesitter is loaded first
+    after = { "nvim-treesitter", "nvim-autopairs" }, -- Ensures correct load order
+  }
 }
